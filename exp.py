89c0297fb27a10d4eb36a77933b9fa314636771e
Matthew Tjandera <74943977+ownerofanime@users.noreply.github.com>
2025-06-05 20:44:18 +0700

    Delete exp.py

--- exp.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-import tkinter
-
-window = Tk()
-entry = tkinter.Entry(window, width = 100, font =('arial', 20))
-entry.pack()
-button = tkinter.Button(window, font=('arial, 20'), text= 'click me!')
-button.pack()
-window.mainloop()
